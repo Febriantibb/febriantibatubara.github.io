5a88d29473cf5324c1dec61d90bd9ecdc613c1d5
Febrianti Sepentina Batubara <101541111+Febriantibb@users.noreply.github.com>
2022-05-30 09:10:27 +0700

    Add files via upload

---
+++ action.php
@@ -0,0 +1,17 @@
+<?php
+$name =$_POST["name"];
+$sapaan = $_POST["sapaan"];
+$email =$_POST["email"];
+$comment =$_POST["comment"];
+
+echo "The Data Your Input Is :<br>";
+echo "$name";
+echo"</br>;
+echo"$sapaan";
+echo"</br>";
+echo"$email";
+echo"</br>;
+echo"$comment";
+echo"</br";
+
+?>
---
+++ index.php.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Membuat Buku Tamu</title>
+</head>
+<body>
+    <strong>Formulir Buku Tamu</strong>
+    <form action="action.php" method="post">
+</head>
+<table>
+<tr>
+<td>Nama</td>
+<td><input type="text" name="name"></td></tr>
+<tr>
+<td>Sapaan</td>
+<td><select name="sapaan">
+<option value=B selected>Sapaan
+<option value="Bapak">Bapak
+<option value="Ibu">Ibu
+<option value="Sdr">Sdr 
+<option value="Sdri">Sdri</td></tr>
+<tr>
+<td>Email</td>
+<td><input type="text" name="email"></td></tr>
+<tr>
+<td>Comment</td>
+<td><textarea name="comment" rows=8 cols=35>
+</textarea></td></tr>
+</tr>
+<td colspan=2 ><input type="submit"
+name="submit" value="submit">
+</td></tr>
+</td></tr></table>
+</td></tr></table>
+</form>
+</body>
+</html>
